3a3739df8e1215799443b4244147862702551792
RochaEverton <rochaevertondev@gmail.com>
2025-09-30 10:41:12 -0300

    Create Navbar with Routes/Links Home and Docs  Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/documents/documents.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documents',
+  imports: [CommonModule],
+  templateUrl: './documents.component.html',
+  styleUrl: './documents.component.scss'
+})
+export class DocumentsComponent {
+
+}
